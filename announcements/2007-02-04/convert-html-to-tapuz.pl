df6853eae9e09a33b597245b004f464aa0ea21e7
shlomif <shlomif@124812d1-1e0d-29c7-c697-926a49d246de>
2008-07-31 15:28:17 +0000

    Added the annoucnements directory with all the past announcements so they'll be available online.

---
+++ announcements/2007-02-04/convert-html-to-tapuz.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+
+use utf8;
+
+use Encode;
+
+binmode STDOUT, ":utf8";
+
+my $html = join("", <>);
+
+$html = decode("utf8", $html);
+
+$html =~ s{<a href="([^"]+)">(.*?)</a>}{$2 (|הלינק|$1|סלינק|)}g;
+
+$html =~ s{<p>}{\n}g;
+$html =~ s{</p>}{\n}g;
+
+$html =~ s{\n{2,}}{\n\n}g;
+
+$html =~ s{\A\n+}{};
+$html =~ s{\n+\z}{\n};
+
+print $html;
